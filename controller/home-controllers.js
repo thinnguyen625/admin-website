dabe3b9312b0ed64ea7c2ba4cc7170507064ad06
Nguyen Manh Linh <nmlinh2799@gmail.com>
2020-01-07 16:41:16 +0700

    Top 10 products

--- controller/home-controllers.js
+++ controller/home-controllers.js
@@ -1,5 +1,7 @@
 var Product = require('../models/product');
 var User = require('../models/user');
+var Order = require('../models/order');
+
 var functions = require('./functions');
 
 // exports.index = (req, res) => {
@@ -7,14 +9,35 @@ var functions = require('./functions');
 // }
 
 exports.index = (req, res) => {
-    User.find({})
-    .then(users => {
-       res.render('pages/index',{
-          users: users
-       });
-    })
-    .catch(err => {
-       console.log('Error: ', err);
-       throw err;
-    });
- }
+	User.countDocuments({}) // Count the number of users
+		.then(countUsers => {
+			Order.aggregate([ // Group items and count quantity per order
+				{
+					'$unwind': {
+						'path': '$items'
+					}
+				}, {
+					'$group': {
+						'_id': '$items.name',
+						'totalAmount': {
+							'$sum': '$items.quantity'
+						}
+					}
+				}
+			]).sort({'totalAmount': -1 }).limit(10) // Top 10
+			.then(top10 => {
+				res.render('pages/index', {
+					countUsers: countUsers,
+					top10: top10
+				});
+			})
+			.catch(err => {
+				console.log('Error: ', err);
+				throw err;
+			});
+		})
+		.catch(err => {
+			console.log('Error: ', err);
+			throw err;
+		});
+}
